0f2530aa8e1d381cc77642136d22962df8064629
khushi singh <khushi.singh3_cs.aiml24@gla.ac.in>
2026-02-19 15:01:07 +0530

    Ultrasonic working code added
    Remove unnecessary empty line in comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  * ultrasonic sensor. It sends a 10µs trigger pulse, receives
  * the echo signal, calculates the distance in centimeters,
  * and prints the result to the Serial Monitor.
- *
+ * 
  * Formula Used:
  * Distance (cm) = (Time × Speed of Sound) / 2
  * Speed of Sound ≈ 0.034 cm/µs
